134ca653a3c4e1205a50cc8e582f0c3a8f917b74
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-05-07 19:03:59 +0200

    Footer styling added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
 import React from "react";
 const Footer = props => {
   return (
-    <div>
-      <ul>
-        {props.arrayOfInfo.map((element, index) => {
-          return <li key={index}>{element}</li>;
-        })}
-      </ul>
-    </div>
+    <footer>
+      <div class="text-center py-3">
+        <ul>
+          {props.arrayOfInfo.map((element, index) => {
+            return <li key={index}>{element}</li>;
+          })}
+        </ul>
+      </div>
+    </footer>
   );
 };
 export default Footer;
