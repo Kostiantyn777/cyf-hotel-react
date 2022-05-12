b98edc61e16ac24eaa5f6405c641d0300df206bb
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-05-12 15:15:23 +0200

    Extracted  pizza order to its own Order component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 const Footer = props => {
   return (
     <footer>
-      <div class="text-center py-3">
+      <div className="text-center py-3">
         <ul>
           {props.arrayOfInfo.map((element, index) => {
             return <li key={index}>{element}</li>;
