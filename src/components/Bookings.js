6a033775ab92dd75802854f1bb2d5b41da2476e5
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-05-20 19:31:00 +0200

    Loaded bookings remotely using useEffect hook

--- src/components/Bookings.js
+++ src/components/Bookings.js
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Search from "./Search.js";
 import SearchResults from "./SearchResults";
-import FakeBookings from "../data/fakeBookings.json";
+//import FakeBookings from "../data/fakeBookings.json";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetch("https://cyf-react.glitch.me/")
+      .then(res => res.json())
+      .then(data => {
+        setBookings(data);
+      });
+  }, []);
 
   const search = searchVal => {
     console.info("TO DO!", searchVal);
@@ -15,7 +23,8 @@ const Bookings = () => {
     <div className="App-content">
       <div className="container">
         <Search search={search} />
-        <SearchResults results={bookings} />
+
+        {bookings.length > 0 ? <SearchResults results={bookings} /> : null}
       </div>
     </div>
   );
