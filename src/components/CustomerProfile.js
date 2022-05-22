659368972421a755992e27adda2f1f7e5d9db32d
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-05-22 17:48:42 +0200

    Added more functionality to CustomerProfile component

--- src/components/CustomerProfile.js
+++ src/components/CustomerProfile.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const CustomerProfile = props => {
-  return <div>{props.id ? <>Customer Profile {props.id}</> : ""}</div>;
+const CustomerProfile = ({ id }) => {
+  const [customerProfileData, setCustomerProfileData] = useState("");
+
+  useEffect(() => {
+    id &&
+      fetch(`https://cyf-react.glitch.me/customers/${id}`)
+        .then(res => res.json())
+        .then(data => {
+          setCustomerProfileData(data);
+        });
+  }, [id]);
+
+  return (
+    <div>
+      {id && (
+        <ul>
+          <li>Customer Profile {id}</li>
+          <li>{`Email: ${customerProfileData.email}`}</li>
+          {customerProfileData.vip ? (
+            <li>Status: VIP customer</li>
+          ) : (
+            <li>Status: NO VIP customer</li>
+          )}
+          <li> {`Phone Number: ${customerProfileData.phoneNumber}`}</li>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default CustomerProfile;
